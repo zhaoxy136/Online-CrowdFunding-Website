ce7ecc44e8151522f26ec9ec27ff5e73a7ade873
ftcyan <ft@172-16-208-11.DYNAPOOL.NYU.EDU>
2017-04-18 16:22:42 -0400

    4.18

---
+++ Data Insertion/Request.sql
@@ -0,0 +1,62 @@
+#Request
+
+INSERT INTO Requests (ReqID, ReqName, Description, OwnerID, MinFundValue, MaxFundValue, PostTime, 
+					FundingEndtime, TargetTime, Status, LikeCount, CurSponCount, CurrentFund)
+Values ('86101', 'Behind the Black Widow', 'A musical show on love, friendship and family of the superhero Black Widow. 
+	We want this show to be an engaging, beautiful and emotive piece of theater, that actually says something about the great woman, 
+	and to contribute to the ever-expanding dialogue on superhero‘s psychological pressure.', 'heiguafu@qq.com', '5000', '7000',
+	'2017-01-08 09:00:00', '2017-01-15 21:00:00', '2017-02-08 00:00:00', 'Completed', '9', '3','6000');
+
+INSERT INTO Requests (ReqID, ReqName, Description, OwnerID, MinFundValue, MaxFundValue, PostTime, 
+					FundingEndtime, TargetTime, Status, LikeCount, CurSponCount, CurrentFund)
+Values ('86102', 'Highway to Hollywood', 'Webseries documenting a beautiful actress on a road trip to Hollywood. 
+	She would document every moment and there are 11 target cities she need to hit along the way', 'heiguafu@qq.com', '1000', '2000',
+	'2017-01-16 14:00:00', '2017-1-20 00:00:00', '2017-01-31 00:00:00', 'Completed', '4', '2','1000');
+
+INSERT INTO Requests (ReqID, ReqName, Description, OwnerID, MinFundValue, MaxFundValue, PostTime, 
+					FundingEndtime, TargetTime, Status, LikeCount, CurSponCount, CurrentFund)
+Values ('86103', 'Ma Ma Land', 'I am self-distributing this serial movie of multi-award winning film La La Land. 
+		The story happens in Manhattan thus it is called Ma Ma Land. The money I raise here will go towards prints, trailers, 
+		and movie advertising so that we can spread the word on screenings.', 'ryang@lalaland.com', '8000', '12000',
+	'2017-02-14 14:00:00', '2017-04-07 18:00:00', '2017-07-07 00:00:00', 'Ongoing', '11', '5','14000');
+
+INSERT INTO Requests (ReqID, ReqName, Description, OwnerID, MinFundValue, MaxFundValue, PostTime, 
+					FundingEndtime, TargetTime, Status, LikeCount, CurSponCount, CurrentFund)
+Values ('86104', 'Actors Co-op Easter Show', 'The Young Actors Co-op of California performs "An Easter Day Show" in April. 
+		The sets will be huge! The cast is huge! The budget is small... ', 'heiguafu@qq.com', '4000', '5000',
+	'2017-02-20 14:00:00', '2017-3-20 00:00:00', '2017-04-16 10:00:00', 'Completed', '5', '3','4000');
+
+INSERT INTO Requests (ReqID, ReqName, Description, OwnerID, MinFundValue, MaxFundValue, PostTime, 
+					FundingEndtime, TargetTime, Status, LikeCount, CurSponCount, CurrentFund)
+Values ('86105', 'JiangNan Leather Shoes', 'This is a series of leather shoes set from my hometown JiangNan, a famous city of China.
+		There will be a total of 12 shoes represents the Chinese Zodiac that will be launched throughout the rest of the year. 
+		Such as the Rooster, Dog, and Pig.', 'langfordhaha@outlook.com', '9000', '12000',
+	'2017-01-28 06:00:00', '2017-2-28 09:00:00', '2017-05-16 00:00:00', 'Failed', '2', '1','2000');
+
+INSERT INTO Requests (ReqID, ReqName, Description, OwnerID, MinFundValue, MaxFundValue, PostTime, 
+					FundingEndtime, TargetTime, Status, LikeCount, CurSponCount, CurrentFund)
+Values ('86106', 'Dreamland - A Debut Jazz Album', 'Dreamland is the first debut album of my jazz band, Emma and her squd.
+			For years I have played musical roles in various pictures which trained me as a jazz drummer.
+			This album represents my true musical loves.', 'emmas@lalaland.com', '9000', '12000',
+	'2017-03-17 08:00:00', '2017-5-20 19:00:00', '2017-07-17 00:00:00', 'Funding', '5', '3','6000');
+
+INSERT INTO Requests (ReqID, ReqName, Description, OwnerID, MinFundValue, MaxFundValue, PostTime, 
+					FundingEndtime, TargetTime, Status, LikeCount, CurSponCount, CurrentFund)
+Values ('86107', 'Dreamland Tour - A Jazz Band Tour', 'I firmly believe that the Dreamland album will go off without a hitch in the studio. 
+		After the album release on July 17th, we would launch a tour in five different cities.
+		Any support is greatly appreciated. Let us make this happen together.', 'emmas@lalaland.com', '10000', '15000',
+	'2017-04-01 18:00:00', '2017-7-20 06:00:00', '2017-09-01 00:00:00', 'Funding', '2', '1','2000');
+
+INSERT INTO Requests (ReqID, ReqName, Description, OwnerID, MinFundValue, MaxFundValue, PostTime, 
+					FundingEndtime, TargetTime, Status, LikeCount, CurSponCount, CurrentFund)
+Values ('86108', 'Glowing Jacket','I have spent a lot of time over the past few years looking at different jacket designs.
+			Now I wanted to take the concept of a glow jacket, which is a fashion jacket line 
+			with illumination stuff on the classic jackets.', 'xiaolizi@qq.com', '6000', '9000',
+	'2017-03-01 17:00:00', '2017-5-22 12:00:00', '2017-06-15 00:00:00', 'Funding', '3', '2','3500');
+
+
+
+
+
+
+
---
+++ Data Insertion/Sponsorship.sql
@@ -0,0 +1,74 @@
+#Sponsorship
+
+#proj1
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86101', 'jenniferlaw@yahoo.com', '2017-01-09 12:00:00', '1500', '1922776600982833');
+
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86101', 'dabiaoge@qq.com', '2017-01-11 15:00:00', '2500', '9292378300219493');
+
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86101', 'levvann@gmail.com', '2017-01-14 11:00:00', '2000', '9088228376142234');
+
+#proj2
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86102', 'wastonlady@hotmail.com', '2017-01-17 09:00:00', '1000', '2876565319228374');
+
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86102', 'robincobie@gmail.com', '2017-01-18 17:00:00', '1000', '9388100238451611');
+
+#proj3
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86103', 'yimei@qq.com', '2017-02-19 09:00:00', '3500', '2733678719930998');
+
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86103', 'dabiaoge@qq.com', '2017-02-27 18:00:00', '3000', '9292378300219493');
+
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86103', 'wastonlady@hotmail.com', '2017-03-17 14:00:00', '2000', '2876565319228374');
+
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86103', 'aajuicy@hotmail.com','2017-03-26 11:00:00', '2500', '3822739400802893');
+
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86103', 'cecaptain@qq.com','2017-04-02 13:00:00', '3000', '9112838726730800');
+
+#proj4
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86104', 'cecaptain@qq.com','2017-02-23 12:00:00', '1500', '9112838726730800');
+
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86104', 'yimei@qq.com', '2017-03-09 10:00:00', '1500', '2733678719930998');
+
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86104', 'jenniferlaw@yahoo.com', '2017-03-14 15:00:00', '1000', '1922776600982833');
+
+#proj5 
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86105', 'sofiabbb@gmail.com','2017-02-03 11:00:00', '2000', '9822073917482948');
+
+#proj6 
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86106', 'aajuicy@hotmail.com','2017-03-30 20:00:00', '2500', '3822739400802893');
+
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86106', 'taronkingsman@gmail.com', '2017-04-12 18:00:00', '2000', '9087262256331017');
+
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86106', 'cruise@outlook.com', '2017-05-09 10:00:00', '1500', '2876335430031982');
+
+#proj7
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86107', 'cruise@outlook.com', '2017-06-13 21:00:00', '2000', '2876335430031982');
+
+#proj8
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86108', 'robincobie@gmail.com', '2017-04-21 20:00:00', '1500', '9388100238451611');
+
+INSERT INTO Sponsorship (ReqID, AccountID, PledgeTime, Amount, CreditCardNumber)
+	Values('86108', 'jenniferlaw@yahoo.com', '2017-05-04 22:00:00', '2000', '1922776600982833');
+
+
+
+
+
