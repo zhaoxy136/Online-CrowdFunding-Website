7f67d0acb8ec03ca45da6f5334364b9ef37b23c6
ftcyan <ft@172-16-208-11.DYNAPOOL.NYU.EDU>
2017-04-18 21:17:30 -0400

    Test data sets completed

---
+++ Data Insertion/Reviews.sql
@@ -0,0 +1,25 @@
+#Reviews
+
+
+INSERT INTO Reviews (UID, ProjID, Rating, RateTime, UserReview)
+	VALUES('JenniferLawrence', '86101', '4', '2017-02-02 14:00:00', 'This project is good!');
+INSERT INTO Reviews (UID, ProjID, Rating, RateTime, UserReview)
+	VALUES('DanStevens', '86101', '5', '2017-02-02 17:00:00', 'I agree with Jenn.');
+INSERT INTO Reviews (UID, ProjID, Rating, RateTime, UserReview)
+	VALUES('LukeEvans', '86101', '5', '2017-02-03 22:00:00', 'My pledges worth!');
+
+
+INSERT INTO Reviews (UID, ProjID, Rating, RateTime, UserReview)
+	VALUES('EmmaWatson', '86102', '4', '2017-01-31 18:00:00', 'Well Done!');
+INSERT INTO Reviews (UID, ProjID, Rating, RateTime, UserReview)
+	VALUES('CobieSmulders', '86102', '5', '2017-02-04 09:00:00', 'This is the best project I have seen. Good job!');
+
+
+INSERT INTO Reviews (UID, ProjID, Rating, RateTime, UserReview)
+	VALUES('ChrisEvans', '86104', '4', '2017-04-17 11:30:00', 'Good job!');
+INSERT INTO Reviews (UID, ProjID, Rating, RateTime, UserReview)
+	VALUES('JamesMcAvoy', '86104', '3', '2017-04-17 21:50:00', 'Well, it is not bad.');
+INSERT INTO Reviews (UID, ProjID, Rating, RateTime, UserReview)
+	VALUES('JenniferLawrence', '86104', '5', '2017-04-19 13:15:00', 'This is a good project.');
+
+
---
+++ Data Insertion/Tags.sql
@@ -0,0 +1,51 @@
+#Tags
+
+INSERT INTO Tags (tag) VALUES ('Music');
+
+INSERT INTO Tags (tag) VALUES ('Show');
+
+INSERT INTO Tags (tag) VALUES ('Jazz');
+
+INSERT INTO Tags (tag) VALUES ('Hiphop');
+
+INSERT INTO Tags (tag) VALUES ('Pop');
+
+INSERT INTO Tags (tag) VALUES ('Rock');
+
+INSERT INTO Tags (tag) VALUES ('Movie');
+
+INSERT INTO Tags (tag) VALUES ('Comedy');
+
+INSERT INTO Tags (tag) VALUES ('Drama');
+
+INSERT INTO Tags (tag) VALUES ('Mystery');
+
+INSERT INTO Tags (tag) VALUES ('Sci-Fi');
+
+INSERT INTO Tags (tag) VALUES ('Travel');
+
+INSERT INTO Tags (tag) VALUES ('Culture');
+
+INSERT INTO Tags (tag) VALUES ('Life');
+
+INSERT INTO Tags (tag) VALUES ('Food');
+
+INSERT INTO Tags (tag) VALUES ('Games');
+
+INSERT INTO Tags (tag) VALUES ('Art');
+
+INSERT INTO Tags (tag) VALUES ('Technology');
+
+INSERT INTO Tags (tag) VALUES ('Photography');
+
+INSERT INTO Tags (tag) VALUES ('Entertainment');
+
+INSERT INTO Tags (tag) VALUES ('Education');
+
+INSERT INTO Tags (tag) VALUES ('Books');
+
+INSERT INTO Tags (tag) VALUES ('Dance');
+
+INSERT INTO Tags (tag) VALUES ('Fitness');
+
+
