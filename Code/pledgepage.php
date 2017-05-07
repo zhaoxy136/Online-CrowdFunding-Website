20f5659e70d6c6814dd11dda8623435647393a86
Xiangyu(Barry) Zhao <xz1503@nyu.edu>
2017-05-07 17:07:14 -0400

    Merge code

--- Code/pledgepage.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ft
- * Date: 2017/4/27
- * Time: 下午3:21
- */
--- Code/timeline.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ft
- * Date: 2017/4/26
- * Time: 上午11:40
- */
